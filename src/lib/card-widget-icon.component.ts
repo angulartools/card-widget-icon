332790b2d8e9c1e6303e8b568b8596b5658b7e30
Daniele Espindola <daniele.ssoares@gmail.com>
2025-06-11 16:27:55 -0300

    Update para versão 20 do Angular

--- src/lib/card-widget-icon.component.ts
+++ src/lib/card-widget-icon.component.ts
@@ -1,13 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TranslationPipe, TranslationService } from '@angulartoolsdr/translation';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
-import { DecimalPipe, NgClass, NgIf, NgStyle } from '@angular/common';
+import { DecimalPipe, NgClass, NgStyle } from '@angular/common';
 
 @Component({
     selector: 'lib-card-widget-icon',
     templateUrl: './card-widget-icon.component.html',
     styleUrls: ['./card-widget-icon.component.scss'],
-    imports: [NgClass, NgStyle, NgIf, DecimalPipe, TranslationPipe, NgxSkeletonLoaderModule]
+    imports: [NgClass, NgStyle, DecimalPipe, TranslationPipe, NgxSkeletonLoaderModule]
 })
 export class CardWidgetIconComponent {
 
