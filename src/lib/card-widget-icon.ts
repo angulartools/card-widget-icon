f42fa75d781ac36666aadd692498307d2b75b4ec
Daniele Espindola <daniele.ssoares@gmail.com>
2026-07-16 11:56:41 -0300

    Update Version

--- src/lib/card-widget-icon.ts
+++ src/lib/card-widget-icon.ts
@@ -1,69 +1,38 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ChangeDetectionStrategy, input, output, inject } from '@angular/core';
 import { TranslationPipe, TranslationService } from '@angulartoolsdr/translation';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { Skeleton } from '@angulartoolsdr/shared-utils';
 import { DecimalPipe, NgClass, NgStyle } from '@angular/common';
 
 @Component({
-    selector: 'lib-card-widget-icon',
-    templateUrl: './card-widget-icon.html',
-    styleUrls: ['./card-widget-icon.scss'],
-    imports: [NgClass, NgStyle, DecimalPipe, TranslationPipe, NgxSkeletonLoaderModule]
+  selector: 'lib-card-widget-icon',
+  templateUrl: './card-widget-icon.html',
+  styleUrls: ['./card-widget-icon.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgClass, NgStyle, DecimalPipe, TranslationPipe, Skeleton]
 })
 export class CardWidgetIcon {
 
-  @Input() label: string | undefined;
-  @Input() info: string | undefined;
-  @Input() total: number | undefined;
-  @Input() totalFiltro: number | undefined;
-  @Input() cor: string | undefined;
-  @Input() icon: string | undefined;
+  label = input<string>('');
+  info = input<string>('');
+  total = input<number>(0);
+  totalFiltro = input<number | null>();
+  cor = input<string>('');
+  icon = input<string>('');
 
-  @Input('percentual')
-  set _percentual(value: number) {
-    if (value !== null && value !== undefined) {
-      this.percentual = this.roundNumber(value, 2);
-    }
-  }
+  percentual = input<number>(0);
 
-  @Input('disabled')
-  set _disabled(value: any){
-    this.disabled = value;
-  }
+  disabled = input<boolean>(false);
+  loading = input<boolean>(true);
+  selected = input<boolean>(false);
+  isMaximize = input<boolean>(false);
 
-  @Input('loading')
-  set _loading(value: any){
-    this.loading = value;
-  }
+  onCardClick = output<void>();
 
-  @Input('selected')
-  set _selected(value: any) {
-    this.selecionado = value;
-  }
-
-  @Input('isMaximize')
-  set _isMaximize(value: any) {
-    this.isMaximize = value;
-  }
-
-  loading = true;
-  disabled = false;
-  percentual = 0;
-  isMaximize = false;
-
-  @Output() onCardClick = new EventEmitter();
-  selecionado = false;
-
-  constructor(protected translate: TranslationService){}
+  translate = inject(TranslationService);
 
   cardClick() {
-    if (!this.disabled) {
+    if (!this.disabled()) {
       this.onCardClick.emit();
     }
   }
-
-  roundNumber(num: number, casas: number) {
-    const potencia = Math.pow(10, casas);
-    return (Math.round((num + Number.EPSILON) * potencia) / potencia);
-  }
-
 }
